8510d85f76cc1c575ba92d30048ac4bf385ff594
Tim Andersson <timalexanderandersson@hotmail.com>
2024-11-23 09:49:20 +0000

    change urls in read_api

--- read_api/urls.py
+++ read_api/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('post.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    path('dj_rest_auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('', include('comment.urls')),
 ]
