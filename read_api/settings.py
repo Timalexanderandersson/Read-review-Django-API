2320e353120174507034d0efeca21efcf5a8d090
Tim Andersson <timalexanderandersson@hotmail.com>
2024-11-25 09:54:25 +0000

    change setting back in rest_framwork

--- read_api/settings.py
+++ read_api/settings.py
@@ -77,10 +77,11 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [(
+        'rest_framework.authentication.SessionAuthentication'
+        if 'DEV' in os.environ
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+    )],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
     'DATETIME_FORMAT': '%d %b %Y',
