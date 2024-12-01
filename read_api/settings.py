5fa5daa2d8e9d01657e9a53b25e02bc0e2395f48
Tim Andersson <timalexanderandersson@hotmail.com>
2024-12-01 09:35:07 +0000

    created pagination to 20

--- read_api/settings.py
+++ read_api/settings.py
@@ -83,7 +83,7 @@ REST_FRAMEWORK = {
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     )],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 4,
+    'PAGE_SIZE': 20,
     'DATETIME_FORMAT': '%d %b %Y',
 }
 
