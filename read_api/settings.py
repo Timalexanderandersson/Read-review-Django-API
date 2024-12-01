24d87b744ce3e096f9be3a9bcf32acb2d00cd91c
Tim Andersson <timalexanderandersson@hotmail.com>
2024-12-01 09:44:53 +0000

    change pagination to 100

--- read_api/settings.py
+++ read_api/settings.py
@@ -83,7 +83,7 @@ REST_FRAMEWORK = {
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     )],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
+    'PAGE_SIZE': 100,
     'DATETIME_FORMAT': '%d %b %Y',
 }
 
