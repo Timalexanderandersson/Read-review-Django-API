7c3eed8bbfa752dc3c3a113cfde0dcd1c64710c2
Tim Andersson <timalexanderandersson@hotmail.com>
2025-01-22 14:18:42 +0000

    created class EmailSending in views for sendingmailapp, and def post

--- sendmailapp/views.py
+++ sendmailapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import generics, status, permissions
+from .serializers import SerializersEmailModel
+from .models import EmailModel
+from rest_framework.response import Response
 
-# Create your views here.
+# Class holding the function for sending email to the admin of the website
+class EmailSending(generics.CreateAPIView):
+    serializer_class = SerializersEmailModel
+    queryset = EmailModel.objects.all()
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        serializer = SerializersEmailModel(data=request.data)
+        if serializer.is_valid():
+            mail = serializer.save()
+            self.send_email(mail)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)    
